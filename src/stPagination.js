f72154df01f3a4919fe6d99e29bd08c75a67a954
OZ <normandiggs@gmail.com>
2014-09-11 23:34:42 +0400

    remove prev commit

--- src/stPagination.js
+++ src/stPagination.js
@@ -6,24 +6,26 @@
                 restrict: 'EA',
                 require: '^stTable',
                 scope: {
-                  currentPage:      '=?',
-                  pages:            '=?',
-                  stItemsByPage:    '@',
-                  stDisplayedPages: '@'
+                  currentPage: '=?',
+                  pages:       '=?'
                 },
                 template: '<div class="pagination"><ul class="pagination"><li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li></ul></div>',
                 replace: true,
                 link: function (scope, element, attrs, ctrl) {
 
-                    scope.stItemsByPage = parseInt(scope.stItemsByPage || 10) || 10;
-                    scope.stDisplayedPages = parseInt(scope.stDisplayedPages || 5) || 5;
+                    function isNotNan(value) {
+                        return !(typeof value === 'number' && isNaN(value));
+                    }
+
+                    var itemsByPage = isNotNan(parseInt(attrs.stItemsByPage, 10)) == true ? parseInt(attrs.stItemsByPage, 10) : 10;
+                    var displayedPages = isNotNan(parseInt(attrs.stDisplayedPages, 10)) == true ? parseInt(attrs.stDisplayedPages, 10) : 5;
 
                     scope.currentPage = 1;
                     scope.pages = [];
 
 
                     scope.$watch(function () {
-                            return [ctrl.tableState().pagination, scope.stItemsByPage, scope.stDisplayedPages];
+                            return ctrl.tableState().pagination;
                         },
                         function () {
                             var paginationState = ctrl.tableState().pagination;
@@ -32,12 +34,12 @@
                             var i;
                             scope.currentPage = Math.floor(paginationState.start / paginationState.number) + 1;
 
-                            start = Math.max(start, scope.currentPage - Math.abs(Math.floor(scope.stDisplayedPages / 2)));
-                            end = start + scope.stDisplayedPages;
+                            start = Math.max(start, scope.currentPage - Math.abs(Math.floor(displayedPages / 2)));
+                            end = start + displayedPages;
 
                             if (end > paginationState.numberOfPages) {
                                 end = paginationState.numberOfPages + 1;
-                                start = Math.max(1, end - scope.stDisplayedPages);
+                                start = Math.max(1, end - displayedPages);
                             }
 
                             scope.pages = [];
@@ -52,12 +54,12 @@
 
                     scope.selectPage = function (page) {
                         if (page > 0 && page <= scope.numPages) {
-                            ctrl.slice((page - 1) * scope.stItemsByPage, scope.stItemsByPage);
+                            ctrl.slice((page - 1) * itemsByPage, itemsByPage);
                         }
                     };
 
                     //select the first page
-                    ctrl.slice(0, scope.stItemsByPage);
+                    ctrl.slice(0, itemsByPage);
                 }
             };
         });
