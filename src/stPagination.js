4249e59150d3e959a41a5cf287f4393805abf64b
Chris McKee <ChrisMcKee@users.noreply.github.com>
2014-10-20 11:26:01 +0100

    pass ref to current page back to UI for Page based server side pagination
    Page based rather than record based pagination.

--- src/stPagination.js
+++ src/stPagination.js
@@ -17,6 +17,8 @@
                     scope.stDisplayedPages = scope.stDisplayedPages ? +(scope.stDisplayedPages) : 5;
 
                     scope.currentPage = 1;
+                    ctrl.tableState().pagination.currentPage = 1;
+                    
                     scope.pages = [];
 
                     function redraw() {
@@ -57,6 +59,7 @@
                     //view -> table state
                     scope.selectPage = function (page) {
                         if (page > 0 && page <= scope.numPages) {
+                            ctrl.tableState().pagination.currentPage = page;
                             ctrl.slice((page - 1) * scope.stItemsByPage, scope.stItemsByPage);
                         }
                     };
