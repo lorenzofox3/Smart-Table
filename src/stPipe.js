faf973a837cfa4af65deecb6b83441b7dc857742
Almar <almarmail-info@yahoo.com>
2014-11-16 14:43:26 +0100

    Ensure backwards compatibility

--- src/stPipe.js
+++ src/stPipe.js
@@ -11,7 +11,12 @@ ng.module('smart-table')
                     if (ng.isFunction(scope.stPipe)) {
                         ctrl.preventPipeOnWatch();
                         ctrl.pipe = function () {
-                            scope.stPipe(ctrl.tableState(), ctrl);
+                            var tableState = ctrl.tableState();
+
+                            // for backwards compatibility, make sure tableState.search exists.
+                            tableState.search = tableState.filters.search ? tableState.filters.search : {};
+
+                            scope.stPipe(tableState, ctrl);
                         }
                     }
                 }
