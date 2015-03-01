412594d0aa7ff4c4a8f1070029481b27d151226e
David Yahalomi <davidyaha@gmail.com>
2015-03-01 02:35:40 +0200

    Adding next page and previous page buttons

--- src/smart-table.module.js
+++ src/smart-table.module.js
@@ -1,9 +1,11 @@
 ng.module('smart-table', []).run(['$templateCache', function ($templateCache) {
     $templateCache.put('template/smart-table/pagination.html',
-        '<li ng-hide="currentPage <= 1"><a ng-click="selectPage(currentPage - 1)">&lt;</a></li>' +
-        '<nav ng-if="pages.length >= 2"><ul class="pagination">' +
+        '<nav ng-if="pages.length >= 2">' +
+        '<button id="previous" ng-if="currentPage > 1" ng-click="selectPage(currentPage - 1)">&lt;</button>' +
+        '<ul class="pagination">' +
         '<li ng-repeat="page in pages"  ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
-        '<li ng-hide="currentPage >= pages.length"><a ng-click="selectPage(currentPage + 1)">&gt;</a></li>' +
-        '</ul></nav>');
+        '</ul>' +
+        '<button id="next" ng-if="currentPage < pages.length" ng-click="selectPage(currentPage + 1)">&gt;</button>' +
+        '</nav>');
 }]);
 
