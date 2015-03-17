de4b1636542dd392808c05296afa883b3dce16df
albert <albert.casadessus@gmail.com>
2015-03-17 23:28:45 +0100

    add new template code in  file, build and run tests using gulp

--- dist/smart-table.debug.js
+++ dist/smart-table.debug.js
@@ -2,17 +2,17 @@
 * @version 1.4.9
 * @license MIT
 */
-(function (ng, undefined){
+(function (ng, undefined) {
     'use strict';
 
-ng.module('smart-table', []).ruconn(['$templateCache', function ($templateCache) {
+ng.module('smart-table', []).run(['$templateCache', function ($templateCache) {
     $templateCache.put('template/smart-table/pagination.html',
         '<div class="pagination" ng-if="pages.length >= 2"><ul class="pagination">' +
         '<li ng-if="stFirstPage" ng-class="{disabled: 1==currentPage}"><a ng-click="selectPage(1)">{{ stFirstPage }}</a></li>' +
         '<li ng-if="stPrevPage" ng-class="{disabled: 1==currentPage}"><a ng-click="selectPage(currentPage-1)">{{ stPrevPage }}</a></li>' +
         '<li ng-if="stCollapsedPages && pages[0] > 1"><a ng-click="selectPage(pages[0] - 1)">...</a></li>' +
         '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
-        '<li ng-if="stCollapsedPages && pages[pages.length - 1] < numPages"><a ng-click="selectPage(pages[pages.length -])">...</a></li>' +
+        '<li ng-if="stCollapsedPages && pages[pages.length - 1] < numPages"><a ng-click="selectPage(pages[pages.length - 1])">...</a></li>' +
         '<li ng-if="stNextPage" ng-class="{disabled: numPages==currentPage}"><a ng-click="selectPage(currentPage+1)">{{ stNextPage }}</a></li>' +
         '<li ng-if="stLastPage" ng-class="{disabled: numPages==currentPage}"><a ng-click="selectPage(numPages)">{{ stLastPage }}</a></li>' +
         '</ul></div>');
@@ -395,7 +395,6 @@ ng.module('smart-table')
           for (i = start; i < end; i++) {
             scope.pages.push(i);
           }
-
         }
 
         //table state --> view
--- src/smart-table.module.js
+++ src/smart-table.module.js
@@ -1,7 +1,13 @@
 ng.module('smart-table', []).run(['$templateCache', function ($templateCache) {
     $templateCache.put('template/smart-table/pagination.html',
         '<div class="pagination" ng-if="pages.length >= 2"><ul class="pagination">' +
+        '<li ng-if="stFirstPage" ng-class="{disabled: 1==currentPage}"><a ng-click="selectPage(1)">{{ stFirstPage }}</a></li>' +
+        '<li ng-if="stPrevPage" ng-class="{disabled: 1==currentPage}"><a ng-click="selectPage(currentPage-1)">{{ stPrevPage }}</a></li>' +
+        '<li ng-if="stCollapsedPages && pages[0] > 1"><a ng-click="selectPage(pages[0] - 1)">...</a></li>' +
         '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
+        '<li ng-if="stCollapsedPages && pages[pages.length - 1] < numPages"><a ng-click="selectPage(pages[pages.length - 1])">...</a></li>' +
+        '<li ng-if="stNextPage" ng-class="{disabled: numPages==currentPage}"><a ng-click="selectPage(currentPage+1)">{{ stNextPage }}</a></li>' +
+        '<li ng-if="stLastPage" ng-class="{disabled: numPages==currentPage}"><a ng-click="selectPage(numPages)">{{ stLastPage }}</a></li>' +
         '</ul></div>');
 }]);
 
