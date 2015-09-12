ca23720758c48388b6651ab1b922f57584d0e5bb
Luís Neves <lneves@opensoft.pt>
2015-09-12 18:48:56 +0100

    possibility to keep track of the selected rows

--- src/stSelectRow.js
+++ src/stSelectRow.js
@@ -4,7 +4,8 @@ ng.module('smart-table')
       restrict: 'A',
       require: '^stTable',
       scope: {
-        row: '=stSelectRow'
+        row: '=stSelectRow',
+        rowsSelected: '=?stSelectRowsSelected'
       },
       link: function (scope, element, attr, ctrl) {
         var mode = attr.stSelectMode || stConfig.select.mode;
@@ -17,8 +18,11 @@ ng.module('smart-table')
         scope.$watch('row.isSelected', function (newValue) {
           if (newValue === true) {
             element.addClass(stConfig.select.selectedClass);
+            scope.rowsSelected && scope.rowsSelected.push(scope.row);
           } else {
             element.removeClass(stConfig.select.selectedClass);
+            //Care for IE8, it doesn't support indexOf. One possible solution can be define it in the Array prototype so it will be available in ie8 
+            scope.rowsSelected && scope.rowsSelected.indexOf(scope.row) !== -1 && scope.rowsSelected.splice(scope.rowsSelected.indexOf(scope.row), 1);
           }
         });
       }
