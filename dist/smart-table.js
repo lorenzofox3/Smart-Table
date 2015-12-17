1379ba779c53510d7ddf20cd159572d7e3e5d97e
ByJC <jeancharles.fauvin@gmail.com>
2015-12-17 18:24:02 +0100

    indentation for stResetSearch

--- dist/smart-table.js
+++ dist/smart-table.js
@@ -526,21 +526,21 @@ ng.module('smart-table')
 
 ng.module("smart-table")
     .directive("stResetSearch", function() {
-         return {
-                restrict: 'EA',
-                require: '^stTable',
-                link: function(scope, element, attrs, ctrl) {
-                  return element.bind('click', function() {
+        return {
+            restrict: 'EA',
+            require: '^stTable',
+            link: function(scope, element, attrs, ctrl) {
+                return element.bind('click', function() {
                     return scope.$apply(function() {
-                      var tableState;
-                      tableState = ctrl.tableState();
-                      tableState.search.predicateObject = {};
-                      tableState.pagination.start = 0;
-                      return ctrl.pipe();
+                        var tableState;
+                        tableState = ctrl.tableState();
+                        tableState.search.predicateObject = {};
+                        tableState.pagination.start = 0;
+                        return ctrl.pipe();
                     });
-                  });
-                }
-              };
+                });
+            }
+        };
     });
 
 })(angular);
