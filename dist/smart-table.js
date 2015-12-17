b319bef6df8e47f346fca8ae2903426d3452bd23
ByJC <jeancharles.fauvin@gmail.com>
2015-12-17 18:09:03 +0100

    add stResetSearch (clear all filters)

--- dist/smart-table.js
+++ dist/smart-table.js
@@ -524,4 +524,23 @@ ng.module('smart-table')
     };
   }]);
 
+ng.module("smart-table")
+    .directive("stResetSearch", function() {
+         return {
+                restrict: 'EA',
+                require: '^stTable',
+                link: function(scope, element, attrs, ctrl) {
+                  return element.bind('click', function() {
+                    return scope.$apply(function() {
+                      var tableState;
+                      tableState = ctrl.tableState();
+                      tableState.search.predicateObject = {};
+                      tableState.pagination.start = 0;
+                      return ctrl.pipe();
+                    });
+                  });
+                }
+              };
+    });
+
 })(angular);
