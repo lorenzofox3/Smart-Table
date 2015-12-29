569aa358ae67cbd0ba185946a949b3e0e7696406
jodinathan <jonathan@jode.com.br>
2015-12-29 17:21:00 -0200

    added ng-model funcionality
    I need to use the $formatters of my ng-model with the smart table and had to tweak it to work.
    Basically added ng-model in the require option and created an if statement to use the model of the event to bind the value.
    
    Thanks for the awesome plugin

--- src/stSearch.js
+++ src/stSearch.js
@@ -1,43 +1,62 @@
 ng.module('smart-table')
   .directive('stSearch', ['stConfig', '$timeout','$parse', function (stConfig, $timeout, $parse) {
     return {
-      require: '^stTable',
-      link: function (scope, element, attr, ctrl) {
+      require: ['^stTable', '^?ngModel'],
+      link: function (scope, element, attr, ctrls) {
+        var ctrl = ctrls[0], ngm = ctrls[1];
         var tableCtrl = ctrl;
         var promise = null;
         var throttle = attr.stDelay || stConfig.search.delay;
         var event = attr.stInputEvent || stConfig.search.inputEvent;
 
-        attr.$observe('stSearch', function (newValue, oldValue) {
-          var input = element[0].value;
-          if (newValue !== oldValue && input) {
-            ctrl.tableState().search = {};
-            tableCtrl.search(input, newValue);
-          }
-        });
+        if (ngm){
+	        scope.$watch(function () {
+				    return ngm.$modelValue;
+			    }, function (newValue, oldValue) {
+				    if (newValue === oldValue)
+					  return;
 
-        //table state -> view
-        scope.$watch(function () {
-          return ctrl.tableState().search;
-        }, function (newValue, oldValue) {
-          var predicateExpression = attr.stSearch || '$';
-          if (newValue.predicateObject && $parse(predicateExpression)(newValue.predicateObject) !== element[0].value) {
-            element[0].value = $parse(predicateExpression)(newValue.predicateObject) || '';
-          }
-        }, true);
-
-        // view -> table state
-        element.bind(event, function (evt) {
-          evt = evt.originalEvent || evt;
-          if (promise !== null) {
-            $timeout.cancel(promise);
-          }
-
-          promise = $timeout(function () {
-            tableCtrl.search(evt.target.value, attr.stSearch || '');
-            promise = null;
-          }, throttle);
-        });
-      }
+				    //console.log('no model', ngm, 'new', newValue, 'old', oldValue);
+				    if (promise !== null) {
+					    $timeout.cancel(promise);
+				    }
+				
+				    promise = $timeout(function () {
+					    tableCtrl.search(newValue, attr.stSearch || '');
+					    promise = null;
+				    }, throttle);
+			    });
+        } else {
+          attr.$observe('stSearch', function (newValue, oldValue) {
+            var input = element[0].value;
+            if (newValue !== oldValue && input) {
+              ctrl.tableState().search = {};
+              tableCtrl.search(input, newValue);
+            }
+          });
+  
+          //table state -> view
+          scope.$watch(function () {
+            return ctrl.tableState().search;
+          }, function (newValue, oldValue) {
+            var predicateExpression = attr.stSearch || '$';
+            if (newValue.predicateObject && $parse(predicateExpression)(newValue.predicateObject) !== element[0].value) {
+              element[0].value = $parse(predicateExpression)(newValue.predicateObject) || '';
+            }
+          }, true);
+  
+          // view -> table state
+          element.bind(event, function (evt) {
+            evt = evt.originalEvent || evt;
+            if (promise !== null) {
+              $timeout.cancel(promise);
+            }
+  
+            promise = $timeout(function () {
+              tableCtrl.search(evt.target.value, attr.stSearch || '');
+              promise = null;
+            }, throttle);
+          });
+        }
     };
   }]);
