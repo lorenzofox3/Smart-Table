abb0a9d5ef0e9956f730028eb3b1786e316abfa2
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-03-22 18:00:34 -0400

    Add [href] to anchor tag in pagination template

--- src/smart-table.module.js
+++ src/smart-table.module.js
@@ -1,7 +1,7 @@
 ng.module('smart-table', []).run(['$templateCache', function ($templateCache) {
     $templateCache.put('template/smart-table/pagination.html',
         '<nav ng-if="numPages && pages.length >= 2"><ul class="pagination">' +
-        '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
+        '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a href="javascript: void(0);" ng-click="selectPage(page)">{{page}}</a></li>' +
         '</ul></nav>');
 }]);
 
